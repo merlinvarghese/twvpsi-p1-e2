1a2564501107f3c416ba08c5a036a97b44505317
chitralekhak <chitra.kunnan@gmail.com>
2019-08-29 15:23:28 +0530

    [chitra,vidya] update. Probability and probability test

--- src/main/Probability.java
+++ src/main/Probability.java
@@ -1,21 +1,21 @@
 //Represent the occurance of an event
 
 class Probability {
-    private float maxChances;
-    private float occurance;
+    private double occurance;
 
-    Probability(float maxChances, float occurance) {
-        this.maxChances = maxChances;
+    Probability(double occurance) {
         this.occurance = occurance;
     }
 
-    boolean equals(Probability probabilityToCompare) {
-        float value1 = this.occurance / this.maxChances;
-        float value2 = probabilityToCompare.occurance / probabilityToCompare.maxChances;
-        System.out.println(value1+","+value2);
-        if (value1 != value2) {
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj == null || obj.getClass() != this.getClass()) {
             return false;
+        } else {
+            Probability probabilityToCompare = (Probability) obj;
+            return this.occurance == probabilityToCompare.occurance;
         }
-        return true;
     }
 }
--- src/test/ProbabilityTest.java
+++ src/test/ProbabilityTest.java
@@ -1,23 +1,40 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SuppressWarnings("ALL")
 class ProbabilityTest {
     @Test
-    void expectTrueFor1_2And3_6Probability() {
-        Probability probability1 = new Probability(2, 1);
-        Probability probability2 = new Probability(6, 3);
-        Assertions.assertTrue(probability1.equals(probability2));
+    void expectTrueFor0_5And0_5Probability() {
+        Probability probability = new Probability(0.5);
+        assertTrue(probability.equals(probability));
     }
+
     @Test
-    void expectFalseFor1_2And2_6Probability() {
-        Probability probability1 = new Probability(2, 1);
-        Probability probability2 = new Probability(6, 2);
-        Assertions.assertFalse(probability1.equals(probability2));
+    void expectFalseFor0_3AndNullProbability() {
+        Probability probability1 = new Probability(0.3);
+        assertFalse(probability1.equals(null));
     }
+
     @Test
-    void expectTrueFor1_3And2_6Probability() {
-        Probability probability1 = new Probability(3, 1);
-        Probability probability2 = new Probability(6, 2);
-        Assertions.assertTrue(probability1.equals(probability2));
+    void expectTrueFor0_3ProbabilityAndValue() {
+        Probability probability1 = new Probability(0.3);
+        assertFalse(probability1.equals(2));
+    }
+
+    @Test
+    void expectTrueFor0_6And0_6Probability() {
+        Probability probability1 = new Probability(0.6);
+        Probability probability2 = new Probability(0.6);
+        assertTrue(probability1.equals(probability2));
+    }
+
+    @Test
+    void expectFalseFor0_2And0_6Probability() {
+        Probability probability1 = new Probability(0.2);
+        Probability probability2 = new Probability(0.6);
+        assertFalse(probability1.equals(probability2));
     }
 }
