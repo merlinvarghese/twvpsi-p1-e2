937f4bd4ed03c08c04e6bec1e2a15c09454031f2
chitralekhak <chitra.kunnan@gmail.com>
2019-08-29 14:25:55 +0530

    [chitra,vidya] Updated. added equals functioanlity and test cases

--- src/main/Probability.java
+++ src/main/Probability.java
@@ -1,15 +1,21 @@
 //Represent the occurance of an event
 
 class Probability {
-    private int maxChances;
-    private int occurance;
+    private float maxChances;
+    private float occurance;
 
-    Probability(int maxChances, int occurance) {
+    Probability(float maxChances, float occurance) {
         this.maxChances = maxChances;
         this.occurance = occurance;
     }
 
     boolean equals(Probability probabilityToCompare) {
+        float value1 = this.occurance / this.maxChances;
+        float value2 = probabilityToCompare.occurance / probabilityToCompare.maxChances;
+        System.out.println(value1+","+value2);
+        if (value1 != value2) {
+            return false;
+        }
         return true;
     }
 }
--- src/test/ProbabilityTest.java
+++ src/test/ProbabilityTest.java
@@ -4,8 +4,20 @@ import org.junit.jupiter.api.Test;
 class ProbabilityTest {
     @Test
     void expectTrueFor1_2And3_6Probability() {
-        Probability probability1 = new Probability(1, 2);
-        Probability probability2 = new Probability(3, 6);
+        Probability probability1 = new Probability(2, 1);
+        Probability probability2 = new Probability(6, 3);
+        Assertions.assertTrue(probability1.equals(probability2));
+    }
+    @Test
+    void expectFalseFor1_2And2_6Probability() {
+        Probability probability1 = new Probability(2, 1);
+        Probability probability2 = new Probability(6, 2);
+        Assertions.assertFalse(probability1.equals(probability2));
+    }
+    @Test
+    void expectTrueFor1_3And2_6Probability() {
+        Probability probability1 = new Probability(3, 1);
+        Probability probability2 = new Probability(6, 2);
         Assertions.assertTrue(probability1.equals(probability2));
     }
 }
